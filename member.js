ef2aa2c794e1af8fa580f4cfdcf4011002a43989
elmo-liveresia <169067730+elmo-liveresia@users.noreply.github.com>
2024-05-23 14:11:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skillsMember()    // Function to show the skills of the member
+{
+    var skills = ['Machine Learning', 'Deep Learning', 'Computer Vision', 'Natural Language Processing'];
+    return skills;
+}
